60759fa09b6f4c326a27c0a1f47e403c7b5317b8
Jin Oh <joh@mymail.mines.edu>
2014-10-15 17:49:56 -0600

    Modified computer player

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -3,9 +3,11 @@ package clueGame;
 import java.util.*;
 
 public class ComputerPlayer extends Player {
+	private ArrayList<Card> seenCards;
 	
 	public ComputerPlayer(String name, String color, int row, int col) {
 		super(name,color,row,col);
+		seenCards= new ArrayList<Card>();
 	}
 	
 	private char lastRoomVisited;
@@ -19,7 +21,7 @@ public class ComputerPlayer extends Player {
 	}
 	
 	public void updateSeen(Card seen) {
-		
+		seenCards.add(seen);
 	}
 	
 }
